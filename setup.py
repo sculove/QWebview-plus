eb340b66b4129574ea1f0d84bd383d43e915f88f
sculove <sculove@gmail.com>
2021-11-12 22:58:49 +0900

    :fire: setup 파일 제거

--- setup.py
+++
@@ -1,8 +0,0 @@
-import sys
-from cx_Freeze import setup, Executable
-
-setup(  name = "QWebview-plus",
-        version = "1.0",
-        description = "QWebview supports Kiwoom Open API+ for JavaScript",
-        author = "sculove",
-        executables = [Executable("qwebviewplus.py", base="Win32GUI")])
